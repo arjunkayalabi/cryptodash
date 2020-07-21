a5e3c2bd1bff436d9a9820c56f061dab958f5ecf
Arjun Umathanu <arjun.umathanu@gmail.com>
2020-07-21 15:57:33 +0530

    Added Styled-Component and Semantic-UI

---
+++ src/components/welcome.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Welcome = ({ name }) => {
+	return (
+		<div>
+			<h1>Hello React World!</h1>
+			<h2>Welcome to {name}!</h2>
+		</div>
+	);
+};
+
+export default Welcome;
